264b439da477bef1ed8bcacc7cf3930d420de7df
Shivam Singh <shivam.alka03@gmail.com>
2024-01-07 18:55:31 +0530

    -apractise

--- searching2.java
+++ searching2.java
@@ -1,5 +1,25 @@
 public class searching2 {
+
+    static boolean binarysearch(int[]arr,int target){
+        int n=arr.length;
+        int st=0,end=n-1;
+        while(st<end){
+            int mid=(st+end)/2;
+            if(target==arr[mid]){
+                return true;
+            }
+            else if(target<mid){
+                end=mid-1;
+            }else {
+                st=mid+1;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
+        int [] a={1,2,3,4,5,6,7};
+        int target=2;
+        System.out.println(binarysearch(a,target));
         
     }
 }
