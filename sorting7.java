c275ca08f1d521b0543deccb7ac641a70bc9a37f
Shivam Singh <shivam.alka03@gmail.com>
2024-01-13 21:15:52 +0530

    -apractise

--- sorting7.java
+++ sorting7.java
@@ -1,4 +1,23 @@
 public class sorting7 {
+    static int findmax(int[]arr){
+        int n=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>n){
+                n=arr[i];
+            }
+        }
+        return n;
+    }
+    static  void countsort(int[]arr){
+        int n=findmax(arr);
+        int[]count=new int[n];
+        for(int i=0;i<n;i++){
+            count[arr[i]]++;
+        }
+
+    }
+
     public static void main(String[] args) {
+        int []arr={6,5,4,6,7,2,7,9};
     }
 }
