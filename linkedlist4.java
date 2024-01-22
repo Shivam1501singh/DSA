c4894f2b2df313decc967bdeceecf7c18927e5ee
Shivam Singh <shivam.alka03@gmail.com>
2024-01-23 00:30:25 +0530

    practise

--- linkedlist4.java
+++ linkedlist4.java
@@ -5,6 +5,30 @@ public class linkedlist4 {
         node(int val){
             this.val=val;
         }
+
+    }
+    public  static class llkd{
+        void display(node head){
+            node temp=head;
+            while(temp!=null){
+                System.out.print(temp.val+"->");
+                temp=temp.next;
+            }
+        }
+        void delmid(node head){
+            node slow=head;
+            node fast=head;
+            if(fast.next==null){
+                System.out.print("it has only one element ");
+                return;
+            }
+            while(fast.next.next.next!=null && fast.next.next!=null){
+                // delete the right middle in case of even linked list//
+                slow=slow.next;
+                fast=fast.next.next;
+            }
+            slow.next=slow.next.next;
+        }
     }
     public static void main(String[] args) {
         node a=new node(1);
@@ -16,6 +40,12 @@ public class linkedlist4 {
         b.next=c;
         c.next=d;
         d.next=e;
+        llkd p=new llkd();
+        p.display(a);
+        System.out.println();
+        p.delmid(a);
+        System.out.println();
+        p.display(a);
 
 
     }
