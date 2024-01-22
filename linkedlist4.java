ae6640fa3e7740d45428307b82d2fcfc2dd57061
Shivam Singh <shivam.alka03@gmail.com>
2024-01-22 23:06:54 +0530

    practise

--- linkedlist4.java
+++ linkedlist4.java
@@ -1,2 +1,22 @@
 public class linkedlist4 {
+    public static class node{
+        int val;
+        node next;
+        node(int val){
+            this.val=val;
+        }
+    }
+    public static void main(String[] args) {
+        node a=new node(1);
+        node b=new node(2);
+        node c=new node(3);
+        node d=new node(4);
+        node e=new node(5);
+        a.next=b;
+        b.next=c;
+        c.next=d;
+        d.next=e;
+
+
+    }
 }
