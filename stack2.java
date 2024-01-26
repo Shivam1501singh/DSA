f23dcffc0c912d0ab4b9d5078ed5a55dca0138b3
Shivam Singh <shivam.alka03@gmail.com>
2024-01-27 00:36:21 +0530

    practise

--- stack2.java
+++ stack2.java
@@ -1,5 +1,18 @@
+import java.util.Stack;
+
 public class stack2 {
     public static void main(String[] args) {
-        
+        Stack<Integer> st=new Stack<>();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        st.push(5);
+        System.out.println(st);
+        Stack<Integer>tp=new Stack<>();
+        while(st.size()!=0){
+            tp.push(st.pop());
+        }
+        System.out.println(tp);
     }
 }
--- stacks1.java
+++ stacks1.java
@@ -1,5 +1,18 @@
+import java.util.Stack;
+
 public class stacks1 {
     public static void main(String[] args) {
-        
+        Stack<Integer> st=new Stack<>();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        st.push(5);
+        st.push(6);
+        System.out.println(st.peek());
+        st.pop();
+        System.out.println(st);
+
+
     }
 }
