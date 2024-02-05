64034ba4b18adeddae85845104f6db572ea26f0b
Shivam Singh <shivam.alka03@gmail.com>
2024-02-05 22:15:11 +0530

    practise

---
+++ queue.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class queue {
+    public static void main(String[] args) {
+        Queue<Integer>st=new LinkedList<>();
+        st.add(1);
+        st.add(2);
+        st.add(3);
+        st.add(4);
+        st.add(5);
+        System.out.println(st);
+        st.poll();
+        System.out.println(st);
+        st.element();
+
+
+
+
+
+    }
+
+}
